f2aa392c5b97fe20deb71f9b37f3450200d61656
Administrator <zqkaiyu@163.com>
2019-01-07 18:27:37 +0800

    test

--- python实战一期/1.python基础/流程控制-for/for.py
+++ python实战一期/1.python基础/流程控制-for/for.py
@@ -44,3 +44,72 @@
 # print(sum)
 
 
+# import random
+# ran = random.randint(1,20)
+# for i in range(1,7):
+#     num = int(input("please input a number: "))
+#     if num == ran:
+#         print('你赢了')
+#         break
+#     else:
+#         if num >ran:
+#             if 6-i != 0: print("太大了")
+#         elif num <ran:
+#             if 6-i != 0: print("太小了")
+# else:
+#     print('你输了')
+
+
+
+# import random
+# ran = random.randint(1,20)
+# for i in range(1,7):
+#     while True:
+#         num = input("please input a number: ")
+#         if num.isdigit():
+#             break
+#         else:
+#             continue
+#     num = int(num)
+#     if num == ran:
+#         print("you win")
+#         break
+#     else:
+#         if num > ran:
+#             if 6-i !=0 :print('too large')
+#             if 6 - i != 0: print('you have '+ str(6-i) + ' test')
+#         elif num < ran:
+#             if 6 - i != 0: print('too small')
+#             if 6 - i != 0: print('you have ' + str(6-i) + ' test')
+# else:
+#     print('you lose')
+
+
+# for i in range(1,5):
+#     if i == 3:
+#         continue
+#     if i == 4:
+#         break
+#     print(i)
+# else:
+#     print('main end')
+
+
+# import sys
+# for i in range(1,11):
+#     if i == 3:
+#         continue
+#     if i == 8:
+#         sys.exit()
+#     print(i)
+# else:
+#     print('end')
+
+
+
+# import time
+# for i in range(1,10):
+#     print(i)
+#     time.sleep(1)
+# else:
+#     print('main end')
