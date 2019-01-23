d4bcb514e7a29bc6abd9cb60bc96f02b7529c5e8
Gavin <zqkaiyu@163.com>
2019-01-23 23:13:46 +0800

    create dir

---
+++ python实战一期/python中级篇/1.揭秘python编程技巧/使用optparse模块实现wc命令.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# encoding: utf-8
+'''
+@author:Gavin
+@contact: zqkaiyu@163.com
+@file: 使用optparse模块实现wc命令.py
+@time: 2019/1/20 10:04 AM
+
+'''
+#! /usr/bin/python
+from optparse import OptionParser
+import sys
+import os
+
+parser = OptionParser("Usage:%prog [file1] [file2]...")
+parser.add_option
